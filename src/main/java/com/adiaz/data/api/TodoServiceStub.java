10e88d71610d6c7f2aa56b9e45c5c4f03b4dd639
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2017-10-19 15:20:37 +0200

    step 07

--- src/main/java/com/adiaz/data/api/TodoServiceStub.java
+++ src/main/java/com/adiaz/data/api/TodoServiceStub.java
@@ -12,4 +12,8 @@ public class TodoServiceStub implements TodoService {
 	public List<String> retrieveTodos(String user) {
 		return Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
 	}
+
+	public void deleteTodo(String todo) {
+
+	}
 }
--- src/test/java/com/adiaz/data/api/TodoBusinessImplMockitoTest.java
+++ src/test/java/com/adiaz/data/api/TodoBusinessImplMockitoTest.java
@@ -1,6 +1,7 @@
 package com.adiaz.data.api;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -34,5 +36,18 @@ public class TodoBusinessImplMockitoTest {
 		given(todoService.retrieveTodos("Ranga")).willReturn(allTodos);
 		List<String> todos = todoBusinessImpl.retrieveTodosRelatedToSpring("Ranga");
 		assertThat(todos.size(), is(2));
+	}
+
+	@Test
+	public void letsTestDeleteNow() {
+		TodoService todoService = mock(TodoService.class);
+		List<String> allTodos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+		when(todoService.retrieveTodos("Ranga")).thenReturn(allTodos);
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		todoBusinessImpl.deleteTodosNotRelatedToSpring("Ranga");
+		verify(todoService).deleteTodo("Learn to Dance");
+		verify(todoService, Mockito.never()).deleteTodo("Learn Spring MVC");
+		verify(todoService, Mockito.never()).deleteTodo("Learn Spring");
+		verify(todoService, Mockito.times(1)).deleteTodo("Learn to Dance");
 	}
 }
