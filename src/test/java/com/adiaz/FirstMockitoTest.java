9588f69e05b7ca749fdcc0de79039a5c2a28a287
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2017-10-19 12:22:30 +0200

    step 01: creating project structure.

---
+++ src/test/java/com/adiaz/FirstMockitoTest.java
@@ -0,0 +1,16 @@
+package com.adiaz;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Created by toni on 23/07/2017.
+ */
+public class FirstMockitoTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
